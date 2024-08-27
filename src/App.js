f70692d5c4938b195460e070e49a20a772a5074f
dave8git <dave8git@gmail.com>
2024-08-27 13:46:05 +0200

    Add redux and actions added

--- src/App.js
+++ src/App.js
@@ -4,24 +4,12 @@ import { useState } from 'react';
 import shortid from "shortid";
 
 function App() {
-  const [books, setBooks] = useState([ //funkcja use state zwraca tablicę
-    {id: 1, title: 'of Mice and Men', author: 'John Steinbeck' },
-    {id: 2, title: 'the Witcher', author: 'Andrzej Sapkowski'},
-  ]);
-
-  const removeBook = bookId => {
-    setBooks(books.filter(book => book.id !== bookId)); 
-  }
-
-  const addBook = newBook => {
-    setBooks([...books, {id: shortid(), title: newBook.title, author: newBook.author }]);
-  }
 
   return (
     <>
       <h1>Hello to books app!</h1>
-      <BooksList books={books} removeBook={removeBook} />
-      <BooksForm addBook={addBook} />
+      <BooksList />
+      <BooksForm />
     </>
   );
 }
--- src/components/BooksList/BooksList.js
+++ src/components/BooksList/BooksList.js
@@ -1,4 +1,14 @@
-const BooksList = ({ books, removeBook }) => {
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+
+const BooksList = () => {
+    const books = useSelector(state => state.books);
+    const dispatch = useDispatch(); 
+
+    const removeBook = bookId => {
+        dispatch({ type: 'REMOVE_BOOK', payload: bookId})
+    };
+    
     return (
         <ul>
             {books.map(book => <li key={book.id}>{book.title} by {book.author}    <button onClick={() => removeBook(book.id)}>Remove Book!</button></li>)}
